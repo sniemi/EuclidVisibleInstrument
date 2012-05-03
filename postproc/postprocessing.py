5d814492230386b04226494e89b2ea23f4ad8feb
Sami-Matias Niemi <smn2@mssl.ucl.ac.uk>
2012-05-03 13:09:58 +0100

    Working on the post processing tools.

--- postproc/postprocessing.py
+++ postproc/postprocessing.py
@@ -9,9 +9,12 @@ Main code of the Euclid Visible Instrument Simulator
 :contact: smn2@mssl.ucl.ac.uk
 
 :version: 0.1
+
+:TODO: Need to probably define things in quandrants as the input might be a full CCD
 """
 import os, sys, datetime, math
-import simulator.logger as lg
+from optparse import OptionParser
+import logger as lg
 import pyfits as pf
 import numpy as np
 import cdm03
@@ -19,9 +22,10 @@ import cdm03
 
 class PostProcessing():
     """
-    Euclid Visible Instrument postprocessing class.
+    Euclid Visible Instrument postprocessing class. This class allows
+    to add radiation damage (as defined by the CDM03 model) and add
+    readout noise to a simulated image.
     """
-
     def __init__(self):
         """
         Class Constructor.
@@ -54,24 +58,109 @@ class PostProcessing():
         return self.information
 
 
-    def applyReadoutNoise(self, readout=4.5):
+    def writeFITSfile(self, data, output):
         """
-        Applies readout noise. The noise is drawn from a Normal (Gaussian) distribution.
-        Mean = 0.0, and std = sqrt(readout).
+        Write out FITS files using PyFITS.
 
-        :param readout: readout noise [default = 4.5]
-        :type readout: float
+        :param data: data to write to a FITS file
+        :type data: ndarray
+        :param output: name of the output file
+        :type output: string
 
+        :return: None
         """
-        noise = np.random.normal(loc=0.0, scale=math.sqrt(readout),
-                                 size=(self.information['ysize'], self.information['xsize']))
-        self.log.info('Adding readout noise...')
-        self.log.info('Sum of readnoise = %f' % np.sum(noise))
-        self.information['data'] += noise
+        if os.path.isfile(output):
+            os.remove(output)
+
+        #create a new FITS file, using HDUList instance
+        ofd = pf.HDUList(pf.PrimaryHDU())
+
+        #new image HDU
+        hdu = pf.ImageHDU(data=data)
+
+        #update and verify the header
+        hdu.header.add_history('Created by VISsim postprocessing tool at %s' % datetime.datetime.isoformat(datetime.datetime.now()))
+        hdu.verify('fix')
+
+        ofd.append(hdu)
+
+        #write the actual file
+        ofd.writeto(output)
 
 
-    def applyRadiationDamage(self, trapfile, dob=0.0, rdose=1.0e10):
+    def discretisetoADUs(self, data, eADU=3.5, bias=1000.0):
         """
+        Convert floating point arrays to integer arrays and convert to ADUs.
+
+        :param data: data to be discretised to.
+        :type data: ndarray
+        :param eADU: conversion factor (from electrons to ADUs) [default=3.5]
+        :type eADU: int
+        :param bias: bias level in ADUs that will be added
+        :type bias: float
+
+        :return: discretised array in ADUs
+        :rtype: ndarray
+        """
+        #convert to ADUs
+        data /= eADU
+        #add bias
+        data += bias
+
+        datai = data.astype(np.int)
+        datai[datai > 2**16-1] = 2**16-1
+
+        self.log.info('Maximum and total values of the image are %i and %i, respectively' % (np.max(datai),
+                                                                                             np.sum(datai)))
+
+        self.information['ADUs'] = datai
+        return self.information
+
+
+    def applyRadiationDamage(self, data, trapfile, dob=0.0, rdose=1.0e10, iquadrant=0):
+        """
+        Apply radian damage based on FORTRAN CDM03 model. The method assumes that
+        input data covers only a single quadrant defined by the iquadrant integer.
+
+        :param data:
+        :type data: ndarray
+
+        :param trapfile:
+        :type trapfile: str
+
+        :param dob:
+
+        :param rdose: radiation dosage (over the full mission)
+        :type rdose: float
+
+        :param iquandrant: number of the quandrant to process:
+        :type iquandrant: int
+
+        cdm03 - Function signature:
+          sout = cdm03(sinp,iflip,jflip,dob,rdose,in_nt,in_sigma,in_tr,[xdim,ydim,zdim])
+        Required arguments:
+          sinp : input rank-2 array('f') with bounds (xdim,ydim)
+          iflip : input int
+          jflip : input int
+          dob : input float
+          rdose : input float
+          in_nt : input rank-1 array('d') with bounds (zdim)
+          in_sigma : input rank-1 array('d') with bounds (zdim)
+          in_tr : input rank-1 array('d') with bounds (zdim)
+        Optional arguments:
+          xdim := shape(sinp,0) input int
+          ydim := shape(sinp,1) input int
+          zdim := len(in_nt) input int
+        Return objects:
+          sout : rank-2 array('f') with bounds (xdim,ydim)
+
+        :Note: Because Python/NumPy arrays are different row/column based, one needs
+               to be extra careful here. NumPy.asfortranarray will be called to get
+               an array laid out in Fortran order in memory.
+
+        :Note: This is probably not the fastest way to do this, because it now requires
+               transposing the arrays twice. However, at least the CTI tracks go to the
+               right direction.
 
         """
         #read in trap information
@@ -80,20 +169,66 @@ class PostProcessing():
         sigma = trapdata[:, 1]
         taur = trapdata[:, 2]
 
-        ntraps = len(nt)
-
         #call Fortran routine
-        x = cdm03.cdm03(self.information['data'],
-                        self.information['xsize'],
-                        self.information['ysize'],sout,
-                        iflip,jflip,
-                        dob,
-                        rdose,
-                        ntraps,
-                        nt,
-                        sigma,
-                        taur)
+        CTIed = cdm03.cdm03(np.asfortranarray(data.transpose()),
+                            iquadrant%2, iquadrant/2,
+                            dob, rdose,
+                            nt, sigma, taur,
+                            [self.information['xsize'], self.information['ysize'], len(nt)])
+        self.information['CTIed'] = CTIed.transpose()
+        return self.information
 
+
+    def applyReadoutNoise(self, data, readout=4.5):
+        """
+        Applies readout noise. The noise is drawn from a Normal (Gaussian) distribution.
+        Mean = 0.0, and std = sqrt(readout).
+
+        :param data: input data to which the readout noise will be added to
+        :type data: ndarray
+        :param readout: readout noise [default = 4.5]
+        :type readout: float
+
+        :return: updated data, noise image, FITS header, xsize, ysize
+        :rtype: dict
+        """
+        noise = np.random.normal(loc=0.0, scale=math.sqrt(readout),
+            size=(self.information['ysize'], self.information['xsize']))
+
+        self.log.info('Adding readout noise...')
+        self.log.info('Sum of readnoise = %f' % np.sum(noise))
+
+        self.information['readnoised'] = data + noise
+        self.information['readonoise'] = noise
+        return self.information
+
+
+
+
+def processArgs(printHelp=False):
+    """
+    Processes command line arguments.
+    """
+    parser = OptionParser()
+
+    parser.add_option('-i', '--inputfile', dest='inputfile',
+        help="Name of the file to be processed", metavar="string")
+
+    if printHelp:
+        parser.print_help()
+    else:
+        return parser.parse_args()
 
 if __name__ == '__main__':
-    pass
+    opts, args = processArgs()
+
+    if opts.inputfile is None:
+        processArgs(True)
+        sys.exit(1)
+
+    process = PostProcessing()
+    info = process.loadFITS(opts.inputfile)
+    info = process.applyRadiationDamage(info['data'], 'cdm_euclid.dat')
+    info = process.applyReadoutNoise(info['CTIed'])
+    info = process.discretisetoADUs(info['readnoised'])
+    info = process.writeFITSfile(info['ADUs'], 'testCTI.fits')
